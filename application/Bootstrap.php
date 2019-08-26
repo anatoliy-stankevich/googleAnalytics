74c46c11ac036aef717bad5f8dd322eb21bf3e4b
stankevich_anatoly <1985stankevich@gmail.com>
2019-08-26 11:25:09 +0300

    init pagination layout

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    protected function _initPagination()
+    {
+        Zend_Paginator::setDefaultScrollingStyle('Sliding');
+        Zend_View_Helper_PaginationControl::setDefaultViewPartial(
+            'pagination.phtml'
+        );
+    }
 }
 
